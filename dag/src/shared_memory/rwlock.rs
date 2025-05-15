698c7502e96991cc5c3010ad632047c1863890ba
Maria Vladykin <maria_vladykin@gmx.de>
2025-05-15 18:57:33 +0000

    feat: add documentation, update error messages, fix read_unlock() - read_unlock() now no longer assumes that decrementing read_count cannot block. This assumption is false due to its nature as a function which can be called even if no read lock was previously acquired.

--- dag/src/shared_memory/rwlock.rs
+++ dag/src/shared_memory/rwlock.rs
@@ -1,56 +1,82 @@
 use super::semaphore::Semaphore;
-
 use anyhow::{anyhow, Result};
 use std::{thread, time::Duration};
 
+/// Acquire read lock by:
+/// - Decrement write_lock semaphore, thereby write locking and checking that there is no active writer
+/// - Decrement read_count to check whether first reader and correcting read_count if necessary
+/// - Register new reader by incrementing read_count semaphore
+/// - Incrementing write_lock semaphore to unlock write_lock
 pub(crate) fn read_lock(write_lock: &Semaphore, read_count: &Semaphore) -> Result<()> {
-    write_lock.wait().map_err(|e| anyhow!("Failed blocking (decrementing) write_lock: {}", e))?; // are there active writers
-    match read_count.try_wait() {
-        Ok(false) => (), // we are the first reader
-        Ok(true) => {
-            // we are not the first reader
-            // correct the read-count, try_wait has decremented it
-            read_count.post().map_err(|e| anyhow!("Failed incrementing read_lock semaphore: {}", e))?;
-        }
-        Err(e) => return Err(anyhow!("Failed to acquire read_lock: {}", e)),
-    }
-    read_count.post().map_err(|e| anyhow!("Failed incrementing read_lock semaphore: {}", e))?; // increment the read count, we are a new reader
+    // Check if there are active writers
+    write_lock.wait().map_err(|e| anyhow!("Failed locking write_lock semaphore: {}", e))?;
 
-    // give others readers a chance to read
-    // now writers are also allowed, but they check the read_count
-    write_lock.post().map_err(|e| anyhow!("Failed icrementing write_lock semaphore: {}", e))?;
+    // TODO: decide whether this block is necessary
+    match read_count.try_wait() {
+        Ok(false) => (), // First reader
+        Ok(true) => {
+            // Not the first reader
+            // correct the read-count, try_wait has decremented it
+            read_count
+                .post()
+                .map_err(|e| anyhow!("Failed correcting read_count semaphore after decrementing it: {}", e))?;
+        }
+        Err(e) => return Err(anyhow!("Failed decrementing read_count semaphore: {}", e)),
+    }
+
+    // Indicate presence of new reader
+    read_count
+        .post()
+        .map_err(|e| anyhow!("Failed incrementing read_count semaphore to indicate new active reader: {}", e))?;
+
+    // Allow new writers (which have to check read_count) and readers
+    write_lock.post().map_err(|e| anyhow!("Failed unlocking write_lock semaphore: {}", e))?;
 
     Ok(())
 }
 
+/// Release write lock by:
+/// - Decrement read_count to unregister active reader.
 pub(crate) fn read_unlock(read_count: &Semaphore) -> Result<()> {
-    read_count.wait().map_err(|e| anyhow!("{}", e))?; // decrement read-count, this can never block, since we are here
-
-    // test if we are the last reader
+    // Decrement read_count semaphore to unregister reader
     match read_count.try_wait() {
         Ok(false) => {
-            // we are the last reader
+            return Err(anyhow!(
+                "Decrementing read_count semaphore (unregistering a reader), which is equal to 0 and therefore indicating no active readers."
+            ))
         }
+        Ok(true) => (), // Successfully unregistered reader
+        Err(e) => return Err(anyhow!("Failed decrementing read_count semaphore: {}", e)),
+    }
+
+    // TODO: decide whether this block is necessary
+    match read_count.try_wait() {
+        Ok(false) => (), // Last reader
         Ok(true) => {
             // we are not the last reader
             // correct the read count value
-            read_count.post().map_err(|e| anyhow!("Failed incrementing read_lock: {}", e))?;
+            read_count.post().map_err(|e| anyhow!("Failed incrementing read_count: {}", e))?;
         }
         Err(e) => {
-            return Err(anyhow!("Failed to perform non-blocking wait (decrement) on read_lock semaphore: {}", e));
+            return Err(anyhow!("Failed decrementing read_count: {}", e));
         }
     }
 
     Ok(())
 }
 
+/// Acquire write lock by:
+/// - Decrement write_lock semaphore's value if it is greater than 0 (indicating there are current writers);
+///   else block main thread until it is greater than 0 and decrement then.
+/// - Wait until read_count semaphore's value is equal to 0, indicating there are no active readers anymore.
 pub(crate) fn write_lock(write_lock: &Semaphore, read_count: &Semaphore) -> Result<()> {
-    write_lock.wait().map_err(|e| anyhow!("Failed acquiring lock: {}", e))?; // Now I have the permission to write, other readers and writers are blocked, but readers can be still active
+    // Get writing permission, new readers and writers are blocked, but readers can be still active
+    write_lock.wait().map_err(|e| anyhow!("Failed acquiring lock: {}", e))?;
 
-    // Test if there are still readers active
+    // Test if there are still active readers
     'x: loop {
         match read_count.try_wait() {
-            Ok(false) => break 'x, // We have no active readers
+            Ok(false) => break 'x, // No active readers
             Ok(true) => {
                 // There is at least one reader active
                 // Correct the read-count (try_wait has decremented it)
@@ -64,7 +90,11 @@ pub(crate) fn write_lock(write_lock: &Semaphore, read_count: &Semaphore) -> Resu
     Ok(())
 }
 
+/// Release write lock by:
+/// - Increment write_lock semaphore value; a greater than 0 value indicates a writable state to other processes.
 pub(crate) fn write_unlock(write_lock: &Semaphore) -> Result<()> {
+    // TODO: decide if asserting no current readers is necessary (inner state validation check).
+
     write_lock.post().map_err(|e| anyhow!("Failed posting write_lock Semaphore: {}", e))?;
     Ok(())
 }
